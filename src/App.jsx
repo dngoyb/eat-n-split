88f59a449ab5fd28c1a6b3dd238a11aa188c524c
Ngoy banza <dngoyb@gmail.com>
2023-07-18 11:45:17 +0200

    Displaying friends list

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,31 @@ function FriendList() {
 	return (
 		<ul>
 			{friends.map((friend) => (
-				<li key={friend.id}>{friend.name}</li>
+				<Friend key={friend.id} friend={friend} />
 			))}
 		</ul>
+	);
+}
+
+function Friend({ friend }) {
+	return (
+		<li>
+			<img src={friend.image} alt={friend.name} />
+			<h3>{friend.name}</h3>
+
+			{friend.balance < 0 && (
+				<p className='red'>
+					You owe {friend.name} ${Math.abs(friend.balance)}{' '}
+				</p>
+			)}
+			{friend.balance > 0 && (
+				<p className='green'>
+					{friend.name} owes ${friend.balance}{' '}
+				</p>
+			)}
+			{friend.balance === 0 && (
+				<p className=''>You and {friend.name} are even </p>
+			)}
+		</li>
 	);
 }
